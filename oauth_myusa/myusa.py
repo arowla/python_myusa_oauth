b259ec62d084cca76dbb398c943f4a9e3c69a7ba
Alison Rowland <alison.rowland@businessusa.gov>
2013-12-18 15:16:37 -0500

    The great dash to underscore shift

--- oauth_myusa/myusa.py
+++ oauth_myusa/myusa.py
@@ -3,6 +3,6 @@ from social.backends.oauth import BaseOAuth2
 class MyusaOAuth2(BaseOAuth2):
     """ MyUSA OAuth Authentication Backend"""
     name = 'myusa'
-    AUTHORIZATION_URL = 'https://my.usa.gov/sign_in'
-    ACCESS_TOKEN_URL = 'https://my.usa.gov/oauth/authorize'
+    AUTHORIZATION_URL = 'https://my.usa.gov/oauth/authorize'
+    ACCESS_TOKEN_URL = AUTHORIZATION_URL
 
